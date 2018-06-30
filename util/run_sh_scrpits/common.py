eb096ca2f72fe19ea2f661e9e90e8030cbe57e02
Zhou Yaoyang <shinezyy@qq.com>
2018-06-30 12:55:00 +0800

    add running lock
    Change-Id: Ie28722a85a196f8e02db7475ddd65ad8f97ca92f

--- util/run_sh_scrpits/common.py
+++ util/run_sh_scrpits/common.py
@@ -5,8 +5,17 @@ from os.path import join as pjoin
 
 def avoid_repeated(func, outdir, *args, **kwargs):
     func_name = func.__name__
-    ts_name = 'ts-{}'.format(func_name)
 
+    running_lock_name = 'running-{}'.format(func_name)
+    running_lock_file = pjoin(outdir, running_lock_name)
+
+    if os.path.isfile(running_lock_file):
+        print('running lock found in {}, skip!'.format(outdir))
+        return
+    else:
+        sh.touch(running_lock_file)
+
+    ts_name = 'ts-{}'.format(func_name)
     out_ts_file = pjoin(outdir, ts_name)
     if os.path.isfile(out_ts_file):
         out_ts = os.path.getmtime(out_ts_file)
@@ -20,9 +29,13 @@ def avoid_repeated(func, outdir, *args, **kwargs):
     script_ts = os.path.getmtime(script_ts_file)
 
     if out_ts < script_ts:
-        func(*args, **kwargs)
-        sh.touch(out_ts_file)
+        try:
+            func(*args, **kwargs)
+            sh.touch(out_ts_file)
+        except Exception as e:
+            print(e)
+        sh.rm(running_lock_file)
     else:
-        print('{} is older than {}, skipped!'.format(out_ts_file,
+        print('{} is older than {}, skip!'.format(out_ts_file,
             script_ts_file))
 
